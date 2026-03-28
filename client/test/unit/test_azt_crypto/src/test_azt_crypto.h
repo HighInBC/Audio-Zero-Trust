59453fd8590c34cdb3555a9873e46af22ce9413e
Aiden <aiden@local>
2026-03-28 10:09:40 -0700

    Clean up moved test paths and ignore local coverage artifact

--- client/test/unit/test_azt_crypto/src/test_azt_crypto.h
+++
@@ -1,3 +0,0 @@
-#pragma once
-#include "test_azt_registry.h"
-namespace azt_test { void register_test_azt_crypto(Registry& out); }
--- client/test/unit/test_azt_http_api/src/test_azt_http_api.h
+++
@@ -1,3 +0,0 @@
-#pragma once
-#include "test_azt_registry.h"
-namespace azt_test { void register_test_azt_http_api(Registry& out); }
